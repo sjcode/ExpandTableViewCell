d1350ec146952afd443364fa500e5bc3d53779d5
sjcode <su.jian@me.com>
2017-01-22 10:23:04 +0800

    Initial Commit

---
+++ ExpandTableViewCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpandTableViewCell
+//
+//  Created by sujian on 2017/1/22.
+//  Copyright © 2017年 sujian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
